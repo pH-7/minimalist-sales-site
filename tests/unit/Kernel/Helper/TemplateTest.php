517c0b103adff88176cbe1f1109b8f7777b00603
Pierre-Henry Soria <hi@ph7.me>
2021-12-31 17:55:49 +1030

    Add tests

--- tests/unit/Kernel/Helper/TemplateTest.php
+++ tests/unit/Kernel/Helper/TemplateTest.php
@@ -1,2 +1,15 @@
 <?php
+/**
+ * (c) Pierre-Henry Soria <hi@ph7.me>
+ * MIT License - https://opensource.org/licenses/MIT
+ */
 
+declare(strict_types=1);
+
+namespace PHenry\Test\App\Kernel\Helper;
+
+use PHPUnit\Framework\TestCase;
+
+class TemplateTest extends TestCase
+{
+}
