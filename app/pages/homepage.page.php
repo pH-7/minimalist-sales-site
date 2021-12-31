f8aea51688c9d148a1611fa85b2c11797f75489b
Pierre-Henry Soria <hi@ph7.me>
2021-12-31 17:47:11 +1030

    Store products into their own products file :)

--- app/pages/homepage.page.php
+++ app/pages/homepage.page.php
@@ -2,22 +2,7 @@
 
 use PHenry\App\Kernel\Helper\Template;
 
-const PRODUCTS = [
-    [
-        'isVisible' => true,
-        'productName' => 'Red Cup ☕',
-        'productUrl' => '/product',
-        'productImage' => 'https://source.unsplash.com/250x250/?coffee,cup',
-    ],
-    [
-        'isVisible' => false,
-        'productName' => 'Blue Mug',
-        'productUrl' => '/product/mug',
-        'productImage' => '/assets/images/blue-mug.png',
-    ],
-];
-
 echo Template::render('homepage/index', [
-        'products' => PRODUCTS
+        'products' => include dirname(__DIR__) . '/products.php'
     ]
 );
---
+++ app/products.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    [
+        'isVisible' => true,
+        'productName' => 'Red Cup ☕',
+        'productUrl' => '/product',
+        'productImage' => 'https://source.unsplash.com/250x250/?coffee,cup',
+    ],
+    [
+        'isVisible' => false,
+        'productName' => 'Blue Mug',
+        'productUrl' => '/product/mug',
+        'productImage' => '/assets/images/blue-mug.png',
+    ],
+    [
+        'isVisible' => false,
+        'productName' => 'Tea Cup',
+        'productUrl' => '/product/tea',
+        'productImage' => 'https://source.unsplash.com/250x250/?cup,tea',
+    ],
+    [
+        'isVisible' => false,
+        'productName' => 'Fruit Juice',
+        'productUrl' => '/product/juice',
+        'productImage' => 'https://source.unsplash.com/250x250/?juice,fruit',
+    ],
+];
